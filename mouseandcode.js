a48f0ec4db2cc3bd25420a2242836263cc98f9ed
macosta2015 <macosta2015@my.fit.edu>
2024-07-30 22:57:36 -0400

    commit

--- mouseandcode.js
+++ mouseandcode.js
@@ -197,7 +197,9 @@ const waitForEnter = require('./components/waitForEnter.js');
         //Working on (5) Extrude 3 PLAY(2.00)
         ////////////////////////////////////////////////////////////////////////////
         const selector9 = 'div[data-id="zUc3/N14UtKVlcJg"]'; // Replace with the appropriate selector
-        const title9 = '(5) Extrude 3 PLAY(2.00) did not regenerate properly: Select face or sketch region to extrude.<br>2 missing selections';
+        // const title9 = '(5) Extrude 3 PLAY(2.00) did not regenerate properly: Select face or sketch region to extrude.<br>2 missing selections';
+        // const title9 = '(5) Extrude 3 PLAY(2.00) did not regenerate properly: No merge scope selected.< br > 1 missing selection';
+        const title9 = '(5) Extrude 3 PLAY(2.00) did not regenerate properly: No merge scope selected.<br>1 missing selection';
         console.log('Right-clicking on the specified element.');
         const editOptions9 = await performRightClickOptionByTitle(newPage, selector9, title9);
         console.log(editOptions9);
